83637fa38e16828a06fef06c3f92d938668dc577
Stryker <stryker.shaw@fhnw.edu>
2026-02-27 08:14:35 -0700

    feat: added the editor text feild adn text

---
+++ iOS/SS-TodoList/Components/ItemEditorOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  ItemEditorOverlay.swift
+//  SS-TodoList
+//
+//  Created by Student on 2/27/26.
+//
+
+import SwiftUI
+
+struct ItemEditorOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemEditorOverlay()
+}
---
+++ iOS/SS-TodoList/Pages/LoginPage.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPage.swift
+//  SS-TodoList
+//
+//  Created by Student on 2/27/26.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginPage()
+}
